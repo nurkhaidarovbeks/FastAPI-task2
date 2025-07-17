53bb0778d06ed5d58734ba636d2f9b0e373ac923
Beks <bnurkhaidarov@gmail.com>
2025-07-17 18:01:49 +0500

    test15

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,21 +3,21 @@ from fastapi.testclient import TestClient
 
 
 def test_register_user(client: TestClient):
-    response = client.post("/register", json={"username": "testuser", "password": "1234"})
+    response = client.post("/register", json={"username": "testuser", "password": "password"})
     assert response.status_code == 200
     assert response.json()["username"] == "testuser"
 
 
 def test_register_duplicate_user(client: TestClient):
-    response = client.post("/register", json={"username": "testuser", "password": "1234"})
+    response = client.post("/register", json={"username": "testuser", "password": "password"})
     assert response.status_code == 400
 
 
 def test_login_success(client: TestClient):
-    client.post("/register", json={"username": "testuser", "password": "1234"})
+    client.post("/register", json={"username": "testuser", "password": "password"})
 
     # login: используем data, не json
-    response = client.post("/login", data={"username": "testuser", "password": "1234"})
+    response = client.post("/login", data={"username": "testuser", "password": "password"})
     assert response.status_code == 200
     assert "access_token" in response.json()
 
