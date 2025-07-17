7386c043cec94410a6e36fba4f8c9f5e40f08863
Beks <bnurkhaidarov@gmail.com>
2025-07-18 04:14:55 +0500

    test15.7

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,6 +1,11 @@
 import os
+import sys
+
+# Добавляем корень проекта в путь (на уровень выше scripts/)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(file), '..')))
+
 from sqlmodel import SQLModel, create_engine
-from models import *
+from models import *  # теперь должно работать
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
