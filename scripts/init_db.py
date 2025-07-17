0d5d8968fe907112b13d220131348ea1af27d144
Beks <bnurkhaidarov@gmail.com>
2025-07-18 04:12:31 +0500

    test15.6

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,8 +1,12 @@
-from sqlmodel import SQLModel, create_engine
 import os
+from sqlmodel import SQLModel, create_engine
+from models import *
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL not set")
+    raise ValueError("DATABASE_URL is not set")
+
 engine = create_engine(DATABASE_URL)
+print("Creating tables...")
 SQLModel.metadata.create_all(engine)
+print("Done.")
