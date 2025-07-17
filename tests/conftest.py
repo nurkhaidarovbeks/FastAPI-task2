12f86687f5dec5cf361109c3604f21dd40551845
Beks <bnurkhaidarov@gmail.com>
2025-07-17 17:58:37 +0500

    test 15

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, create_engine, Session
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(file), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from main import app
 from database import get_session
